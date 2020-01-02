03fe7b4acb32647a210312f36628276f216b4adf
guoyushi <13521851759@163.com>
2020-01-02 20:42:59 +0800

    需改触摸事件滑动位置不准确的问题

--- src/views/cityChoose/components/cityAlphaber.vue
+++ src/views/cityChoose/components/cityAlphaber.vue
@@ -54,8 +54,8 @@ export default {
         }
         this.timer = setTimeout(() => {
           // const startY = this.$refs['A'][0].offsetTop
-          const touchY = e.touches[0].clientY - 20
-					// console.log(e.touches[0])
+          const touchY = e.touches[0].clientY - 15
+					console.log(e.touches[0])
           const index = Math.floor((touchY - this.startY) / 30)
           if (index >= 0 && index < this.letters.length) {
             this.$emit('change', this.letters[index])
