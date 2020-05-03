36c9ac0ed62dc307b62cd3900d57487513645776
guoyushi <13521851759@163.com>
2020-05-03 10:02:10 +0800

    添加登录页

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,21 +1,188 @@
 <template>
   <div class="login">
-    <p @click="login">登录页</p>
+    <div class="logo">
+      <img src="../../assets/logo.png" alt />
+      <p>方便 快捷 安全</p>
+    </div>
+    <div class="user_info">
+      <van-tabs v-model="active">
+        <van-tab title="账号登录">
+          <van-cell-group>
+            <van-field v-model="phone" type="number" label="手机：" placeholder="请输入手机号" />
+            <van-field v-model="password" type="password" label="密码：" placeholder="请输入密码" />
+          </van-cell-group>
+        </van-tab>
+        <van-tab title="验证码登录">
+          <van-cell-group>
+            <van-field v-model="phone" type="number" label="手机：" placeholder="请输入手机号" />
+            <van-field v-model="password" label="验证码：" placeholder="请输入您的验证码" />
+            <span class="verification_code" @click="getCode()">{{verificationCode}}</span>
+          </van-cell-group>
+        </van-tab>
+      </van-tabs>
+    </div>
+    <div class="login_buttom" @click="login()">登录</div>
+    <div class="login_weixin" @click="weixinLogin()">
+      <img src="../../assets/weixin.png" alt />
+      <span>微信登录</span>
+    </div>
+    <div class="register_or_forget_password">
+      <span class="forget_password" @click="forgetPassword()">忘记密码？</span>
+      <span class="register" @click="register()">注册账号</span>
+    </div>
+    <!--<p @click="login">登录页</p>-->
   </div>
 </template>
 <script>
 export default {
   name: "login",
   data() {
-    return {};
+    return {
+      active: 0,
+      phone: "",
+      password: "",
+      verificationCode: "获取验证码"
+    };
   },
   methods: {
+    register(){
+      console.log('注册账号')
+    },
+    forgetPassword(){
+      console.log('忘记密码');
+    },
+    getCode() {
+      console.log(this.verificationCode);
+    },
+    weixinLogin(){
+      console.log('微信登录')
+    },
     login() {
-      sessionStorage.setItem('isAuthentication',false)
-			this.$router.push('/authentication')
-		}
+      sessionStorage.setItem("isAuthentication", false);
+      this.$router.push("/authentication");
+    }
   }
 };
 </script>
-<style scoped>
+<style lang="less">
+.van-hairline--top-bottom::after,
+.van-hairline-unset--top-bottom::after {
+  border: none;
+}
+.login {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  .logo {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 0.9rem;
+      height: 0.35rem;
+    }
+    p {
+      margin-top: 0.11rem;
+      font-size: 0.14rem;
+      font-family: Helvetica;
+      color: rgba(0, 0, 0, 1);
+      line-height: 17px;
+    }
+  }
+  .user_info {
+    margin: 0 0.46rem;
+    margin-top: 0.6rem;
+    .van-tabs__line {
+      background: #4984f9;
+    }
+    .van-tab {
+      font-size: 0.14rem;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: rgba(51, 51, 51, 1);
+    }
+    .van-tab--active {
+      color: #4984f9;
+    }
+    .van-tabs__content {
+      margin-top: 0.37rem;
+      position: relative;
+    }
+    .van-cell {
+      border-bottom: 1px solid #8c8c8c;
+      padding: 0.1rem 0;
+      .van-field__label {
+        width: 0.6rem;
+      }
+    }
+    .verification_code {
+      font-size: 0.12rem;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: #4984f9;
+      line-height: 17px;
+      position: absolute;
+      right: 0;
+      bottom: 0.14rem;
+    }
+  }
+  .login_buttom {
+    margin: 0 0.46rem;
+    margin-top: 0.4rem;
+    box-sizing: border-box;
+    text-align: center;
+    background: linear-gradient(
+      226deg,
+      rgba(77, 130, 249, 1) 0%,
+      rgba(73, 132, 249, 1) 100%
+    );
+    border-radius: 4px;
+    height: 0.4rem;
+    line-height: 0.4rem;
+    font-size: 0.14rem;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: rgba(255, 255, 255, 1);
+  }
+  .login_weixin {
+    margin: 0 0.46rem;
+    margin-top: 0.17rem;
+    box-sizing: border-box;
+    text-align: center;
+    font-size: 0.14rem;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: rgba(21, 211, 107, 1);
+    border-radius: 4px;
+    border: 1px solid rgba(140, 140, 140, 1);
+    height: 0.4rem;
+    line-height: 0.4rem;
+    img {
+      width: 0.2rem;
+      height: 0.16rem;
+      margin-right: 0.07rem;
+    }
+  }
+  .register_or_forget_password {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 0.16rem;
+    .forget_password {
+      font-size: 0.14rem;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: rgba(102, 102, 102, 1);
+      line-height: 0.2rem;
+    }
+    .register {
+      font-size: 0.14rem;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: #4984F9;
+      line-height: 0.2rem;
+    }
+  }
+}
 </style>
